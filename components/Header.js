62795a4ce9613af2e92f88147c63a16156b29e38
Charlie Finos <charliefinos@Charlies-MacBook-Pro.local>
2022-07-13 07:40:35 -0300

    add tailwind

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,9 @@ export default function Header() {
             {address ? <p>{address}</p> : <button onClick={() => unlock()}>COnnect</button>}
 
             <h1>Holaaa</h1>
+            <div className="w-full bg-red-600">
+                <button className="w-12 bg-blue-600 hover:bg-blue-900 border-r-2">Hola</button>
+            </div>
         </div>
     )
 }
